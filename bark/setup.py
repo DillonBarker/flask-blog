5d4fde6b48a5f15a5691b888a3327aabb394d10d
DillonBarker <barker.dill98@gmail.com>
2020-11-02 16:47:04 +0000

    adding setup.py and manifest to allow project to be installable

---
+++ bark/setup.py
@@ -0,0 +1,12 @@
+from setuptolls import find_packages, setup
+
+setup(
+    name='bark',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
