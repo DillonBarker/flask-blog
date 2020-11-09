9b99a6b2b848ca3ef9d6f64dacda07fa9d82bd15
DillonBarker <barker.dill98@gmail.com>
2020-11-09 12:11:26 +0000

    removing features

--- bark/features/homepage.feature
+++
@@ -1,7 +0,0 @@
-Feature: Website Homepage
-
-    Scenario Outline: Components
-        Given I load the website
-        When I go to "Home" page
-        Then I see this component "<article>"
-        
---
+++ bark/test/test_selenium_webdriver.py
@@ -0,0 +1,6 @@
+import pytest
+import pytest_html
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
